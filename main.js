9be66dc1bacef786b27882626ec1106ac79d8c31
Donald <1376825153@qq.com>
2020-06-26 16:17:46 +0800

    coding from here

---
+++ main.js
@@ -0,0 +1,50 @@
+const app = new PIXI.Application({
+  backgroundColor: 0x1099bb,
+  antialias: true
+});
+let lines = new PIXI.Graphics();
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+document.body.appendChild(app.view);
+
+window.app = app;
+app.renderer.plugins.interaction
+                                .on('pointerdown', onPointerDown)
+                                .on('pointerup', onPointerup)
+                                .on('pointermove', onPointermove)
+let points = [];
+
+function onPointerDown(e) {
+  let x = e.data.global.x,y = e.data.global.y;
+  // points.push({x,y})
+  // console.log(points)
+  // drawLine()
+}
+
+function onPointerup(e){
+  let x = e.data.global.x,y = e.data.global.y;
+  points.push({x,y})
+  console.log(points)
+  drawLine(points)
+}
+
+function onPointermove(e){
+  let x = e.data.global.x,y = e.data.global.y;
+  drawLine(points.concat({x,y}))
+}
+
+function drawLine(points){
+  lines.clear()
+  lines.beginFill(0x1099bb);
+  lines.lineStyle(2, 0xff0000, 1);
+  lines.moveTo(points[0].x, points[0].y);
+  for(let i=1;i<points.length;i++){
+    lines.lineTo(points[i].x, points[i].y);
+  }
+  lines.endFill();
+  app.stage.addChild(lines);
+}
+
