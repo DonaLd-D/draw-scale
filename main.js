c422a294e48e1ab6a9f042090e7bccc582f8bcee
Donald <1376825153@qq.com>
2020-06-27 10:00:53 +0800

    展示线段的长度

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const app = new PIXI.Application({
 });
 let lines = new PIXI.Graphics();
 
+let message = new PIXI.Text();
+message.anchor.x=0.5;
+message.anchor.y=0.5;
+
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
@@ -16,6 +20,7 @@ app.renderer.plugins.interaction
                                 .on('pointerup', onPointerup)
                                 .on('pointermove', onPointermove)
 let points = [];
+let middlePoints=[]
 
 function onPointerDown(e) {
   let x = e.data.global.x,y = e.data.global.y;
@@ -29,26 +34,48 @@ function onPointerup(e){
   if(points.length<2){
     points.push({x,y})
   }
+  countMidPoints(points)
   // console.log(points)
   drawLine(points)
+  
 }
 
 function onPointermove(e){
   let x = e.data.global.x,y = e.data.global.y;
-  if(points.length<2){
+  if(points.length<2 && points.length>0){
     drawLine(points.concat({x,y}))
   }
 }
 
+function countMidPoints(points){
+  for(let i=1;i<points.length;i++){
+    let midX=(points[i].x + points[i-1].x)/2;
+    let midY=(points[i].y + points[i-1].y)/2;
+    middlePoints.push({midX,midY})
+    console.log(middlePoints)
+  }
+}
+
+function showLength(middlePoints){
+  for(let i=0;i<middlePoints.length;i++){
+    message.position.x=middlePoints[i].midX;
+    message.position.y=middlePoints[i].midY;
+    message.text='length'
+  }
+  // console.log(message)
+  app.stage.addChild(message);
+}
+
 function drawLine(points){
   lines.clear()
-  lines.beginFill(0x1099bb);
   lines.lineStyle(2, 0xff0000, 1);
   lines.moveTo(points[0].x, points[0].y);
   for(let i=1;i<points.length;i++){
     lines.lineTo(points[i].x, points[i].y);
   }
   lines.endFill();
+  console.log(lines)
   app.stage.addChild(lines);
+  showLength(middlePoints)
 }
 
