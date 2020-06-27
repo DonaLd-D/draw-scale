4f35ca1f30c595124225fce982b8cd10708955b3
Donald <1376825153@qq.com>
2020-06-27 10:53:41 +0800

    展示线段的数字长度

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let lines = new PIXI.Graphics();
 
 let message = new PIXI.Text();
 message.anchor.x=0.5;
-message.anchor.y=0.5;
+message.anchor.y=0.9;
 
 app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
@@ -20,7 +20,8 @@ app.renderer.plugins.interaction
                                 .on('pointerup', onPointerup)
                                 .on('pointermove', onPointermove)
 let points = [];
-let middlePoints=[]
+let middlePoints=[];
+let lengths=[]
 
 function onPointerDown(e) {
   let x = e.data.global.x,y = e.data.global.y;
@@ -35,9 +36,9 @@ function onPointerup(e){
     points.push({x,y})
   }
   countMidPoints(points)
+  calLength(points)
   // console.log(points)
   drawLine(points)
-  
 }
 
 function onPointermove(e){
@@ -52,17 +53,27 @@ function countMidPoints(points){
     let midX=(points[i].x + points[i-1].x)/2;
     let midY=(points[i].y + points[i-1].y)/2;
     middlePoints.push({midX,midY})
-    console.log(middlePoints)
+    // console.log(middlePoints)
   }
+}
+
+function calLength(points){
+  for(let i=1;i<points.length;i++){
+    let l=Math.pow(points[i].x - points[i-1].x,2) + Math.pow(points[i].y - points[i-1].y,2);
+    let li=Math.abs(Math.sqrt(l));
+    console.log(li)
+    lengths.push(li.toFixed(2))
+  }
+  showLength(middlePoints)
 }
 
 function showLength(middlePoints){
   for(let i=0;i<middlePoints.length;i++){
     message.position.x=middlePoints[i].midX;
     message.position.y=middlePoints[i].midY;
-    message.text='length'
+    message.text=lengths[0]
   }
-  // console.log(message)
+  console.log(message)
   app.stage.addChild(message);
 }
 
@@ -74,8 +85,7 @@ function drawLine(points){
     lines.lineTo(points[i].x, points[i].y);
   }
   lines.endFill();
-  console.log(lines)
+  // console.log(lines)
   app.stage.addChild(lines);
-  showLength(middlePoints)
 }
 
